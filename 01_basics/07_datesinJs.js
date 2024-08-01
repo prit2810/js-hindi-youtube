fc9f5b2506a4af90f5839d93d6eb71741c15c714
prit2810 <124783672+prit2810@users.noreply.github.com>
2024-08-01 07:39:13 +0000

    dates in  js

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,35 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,3,2);
+//  let myCreatedDate = new Date("2023-01-14");
+  let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate =  new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// also people use string interpolation
+// `${newDate.getDay() and the time is ...}`
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
